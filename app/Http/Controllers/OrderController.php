e67d79537dc4ac656116d152823926e9536847c7
Rafif <rafifmurtadho@gmail.com>
2023-07-25 14:16:02 +0800

    v1.0.0

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -37,6 +37,7 @@ class OrderController extends Controller
                 'gross_amount' => $order->total_price,
             ),
             'customer_details' => array(
+                'first_name' => $request->email,
                 'name' => $request->name,
                 'email' => $request->email
             ),
@@ -50,7 +51,7 @@ class OrderController extends Controller
         $serverKey = 'SB-Mid-server-90l-AtNNohLAXQQJ8RkX9aoT';
         $hashed = hash("sha512", $request->order_id.$request->status_code.$request->gross_amount.$serverKey);
         if($hashed == $request->signature_key){
-            if($request->transaction_status == 'capture'){
+            if($request->transaction_status == 'capture' or $request->transaction_status == 'settlement'){
                 $order = Order::find($request->order_id);
                 $order->update(['status' => 'Paid']);
             }
